83d242e147d040fc6608ea731aade4479022c76c
mrg <rgmengedoth@gmail.com>
2023-07-25 11:17:31 +0200

    up

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,31 @@
 import './style.css'
 
+/*
+*   boolean = true or false
+*   number = 1, 2.33, ...
+*       infinity
+*       NaN
+*       bigint
+*   string = "Hello darkness my old friend" or 'Hello darkness my old friend'
+*   Date - new Date()
+*   Object
+*       undefindes
+*       unkown
+*       void
+*       never
+*       null
+*       any
+*   const x = [1, 2, 3, "Hello"]
+*       x.push(10)
+*       x.pop()
+*       x[3]
+*   const user = {name: "john", age: 20}
+*       user.name = "john"
+*       user.name
+*       user.age
+*       user.email = "email@email.com";
+*       user["name"]
+* */
 
+let helloDark = "Hello darkness my old friend"
+console.log(helloDark)
